ad9a81fa4ef287f31cad7f392cf27b7b218daad3
lmaooo-tech <ocoijosephat@gmail.com>
2025-11-15 12:28:48 +0300

    Update weather_advice.py with new logic

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -25,13 +25,13 @@ weather= input("What's the weather like today? (sunny/rainy/cold):")
  #recommend dresscode for user
 
 if weather == "sunny" :
-    print(f"Wear at-shirt and sunglasses.")
+    print(f"Wear a t-shirt and sunglasses.")
 
 elif weather == "rainy" :
-    print(f"Don`t forget your umbrella and a raincoat.")
+    print(f"Don't forget your umbrella and a raincoat.")
 
 elif weather == "cold" :
      print(f"Make sure to wear a warm coat and a scarf.")  
 
 else:
-    print(f"Sorry, I don`t have recommendations for this weather.")
+    print(f"Sorry, I don't have recommendations for this weather.")
