a93107122a35e3acec66b94d380e56e6640d1408
lmaooo-tech <ocoijosephat@gmail.com>
2025-11-21 23:20:31 +0300

    Add the changes

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -13,7 +13,9 @@ def main():
 
         if choice == '1':
             # Prompt for and add an item
-            pass
+            item = input("Enter the item to add: ")
+            shopping_list.append(item)
+            print(f"'{item}' added to the list.")
         elif choice == '2':
             # Prompt for and remove an item
             pass
